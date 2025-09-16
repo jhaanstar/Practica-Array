0b48984d607f2e897102b29c9976758e3321d824
jhaanstar <23308051220759@cbtis122.edu.mx>
2025-09-15 18:22:25 -0600

    Create ejercicio_03.js

---
+++ ejercicio_03.js
@@ -0,0 +1,5 @@
+// Ejercicio 3: Reorganizando los Objetos Mágicos
+// Instrucciones: Invierte el orden de los objetos mágicos.
+const objetosMagicos = [1, 2, 3, 4, 5];
+objetosMagicos.reverse()
+console.log(objetosMagicos)
