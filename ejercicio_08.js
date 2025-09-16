d9421d33d35bd7fbd4581d884b4b847fb5c29f63
jhaanstar <23308051220759@cbtis122.edu.mx>
2025-09-15 18:27:26 -0600

    Create ejercicio_08.js

---
+++ ejercicio_08.js
@@ -0,0 +1,8 @@
+// Ejercicio 8: Verificando si Algún Hechizo es lo Suficientemente Poderoso
+// Instrucciones: Verifica si al menos un hechizo tiene un poder mayor a 100.
+const hechizos = [60, 80, 110, 95, 85];
+// Escribe tu solución aquí usando some():
+let respuesta=hechizos.some(function(hechizos){
+  return (hechizos>100)
+})
+console.log(respuesta)
